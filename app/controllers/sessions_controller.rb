cb7efa37c22ebbd1d8af0a692f5f11b7f13f6b6f
Michael Heilbronner <michaelheilbronner2025@u.northwestern.edu>
2024-05-26 23:30:29 +0000

    progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if @user["password"] == params["password"]
+        flash["notice"] = "You've logged in."
+        redirect_to "/"
+          #/=taking home
+      else
+        flash["notice"] = "Wrong passowrd!"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Bad email!"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
