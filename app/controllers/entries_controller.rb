cd18fa6dbb6a85c2e2483f2481f3523acb2bc985
Michael Heilbronner <michaelheilbronner2025@u.northwestern.edu>
2024-06-03 00:01:26 +0000

    image section

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,16 +2,22 @@ class EntriesController < ApplicationController
 
   def new
   end
-
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["user_id"] = @current_user["id"]
     @entry.save
+  else
+    flash["notice"] = "Login first."
+  end
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
 end
+
